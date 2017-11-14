7e25be342cd6603c9d0be27b436b38c8f59b3a99
Timothy W. Hilton <thilton@ucmerced.edu>
2017-11-14 10:27:24 -0800

    separate __main__ to different file
    - that way can change paths, etc. without messing up branch
      development of plot_diff.py

--- plot_diff.py
+++ plot_diff.py
@@ -280,24 +280,3 @@ def graphics(vd, t_idx=0, layer=None, fig_type='png', domain=2):
     fig.savefig(fname=fname)
     print("done ({})".format(str(datetime.datetime.now() - t0)))
     return(None)
-
-
-if __name__ == "__main__":
-    cscratch = os.path.join('/', 'global', 'cscratch1', 'sd', 'twhilton')
-    ctl_dir = os.path.join(cscratch, 'WRFv3.9_Sensitivity',
-                           'WRFv3.9_Sensitivity_Ctl_short', 'WRFV3',
-                           'run')
-    dry_dir = os.path.join(cscratch, 'WRFv3.9_Sensitivity',
-                           'WRFv3.9_Sensitivity_DrySoil_newrst', 'WRFV3',
-                           'run')
-    vd = var_diff(os.path.join(ctl_dir, 'wrfrst_d01_2009-06-02*'),
-                  os.path.join(dry_dir, 'wrfrst_d01_2009-06-02*'),
-                  label_A='short_ctl',
-                  label_B='short_dry',
-                  varname='QCLOUD')
-    read_data = True
-    if read_data:
-        vd.read_files()
-        # vd.mask_land_or_water(mask_water=True)
-    for this_t in range(0, 1):  # 255
-        fig = graphics(vd, t_idx=this_t, layer=0, domain=1)
