e5bc520bc82fb3a6693c208bf710f3517be29f43
Timothy W. Hilton <thilton@ucmerced.edu>
2017-11-29 08:57:19 -0800

    implement make_all_land_urban()
    - there's an off-by-one error in adjusting LANDUSEF.  Currently PFT 1
      is unaltered, index 14 is set to 1 everywhere (should be index 13,
      and only land pixels), and all index 17 pixels are set to 0
      everywhere (should be 1 for water pixels).

--- adjust_WRF_soil_moisture.py
+++ adjust_WRF_soil_moisture.py
@@ -19,6 +19,51 @@ import glob
 import os
 
 
+def make_all_land_urban(fname):
+    """set all land use in a real.exe wrfinput netcdf file to urban
+
+    real.exe takes meteorological data prepared by WPS and creates
+    wrfinput files.  WPS is the `WRF preprocessing system
+    <http://www2.mmm.ucar.edu/wrf/users/>`.
+
+    ARGS
+    fname (character): full path to a met_em* file produced by the WPS.
+
+    NOTES
+    - The netCDF file is altered in place, so the caller must have
+      write priveleges for the file.
+    - There are several land use datasets available to WRF.  The
+      default is MODIS.  See Ch. 3 of the WRF user guide, section
+      "Selecting Between USGS and MODIS-based Land Use
+      Classifications".  See also file LANDUSE.TBL in the WRF run
+      directory.
+    - Currently doesn't check the landmask data in the netCDF file.
+      It assumes that WPS correctly sets LU_INDEX for all water pixels
+      to the value contained in the ISWATER global netCDF attribute.
+      The value of ISWATER depends on the land use dataset selected.
+    """
+    nc = netCDF4.Dataset(fname, 'a')  # open in append mode
+    # set LU_INDEX to urban for all non-water pixels.  This approach
+    #
+    landuse = nc.variables['LU_INDEX'][...]
+    landuse[np.where(landuse != nc.ISWATER)] = nc.ISURBAN
+    nc.variables['LU_INDEX'][...] = landuse
+    print "modified LU_INDEX in {fname}".format(fname=fname)
+    # LANDUSEF specifies a fraction for all land use types in each
+    # pixel.  Set all urban fraction in LANDUSEF to (1.0 -
+    # LANDUSEF[LU_water]) and all other non-water fractions to 0.0.
+    all_land_use = np.arange(1, nc.NUM_LAND_CAT)
+    non_water_non_urban = np.setdiff1d(all_land_use, [nc.ISWATER, nc.ISURBAN])
+    nc.variables['LANDUSEF'][:, non_water_non_urban, ...] = 0.0
+    non_water_fraction = 1.0 - nc.variables['LANDUSEF'][:, nc.ISWATER, ...]
+    nc.variables['LANDUSEF'][:, nc.ISURBAN, ...] = non_water_fraction
+    # TODO change module name to reflect that it
+    # now can alter landuse
+    nc.history = ("created by metgrid_exe.  All non-waterland use set "
+                  "to urban by adjust_WRF_soil_moisture python module.")
+    nc.close()
+
+
 def reduce_soil_moisture(fname, f, soil_moist_vars=None,
                          land_sea_var='LANDMASK'):
     """multiply WPS soil moisture values in a netcdf file by a constant factor
