8c59971845c1419959a345c42da13e01ef42c7db
Timothy W. Hilton <thilton@ucmerced.edu>
2017-12-04 09:31:01 -0800

    wrf_python_demo: verify that wrf reads U, V with correct dimensions
    - wrf module does the "unstaggering" automatically.  Without
      "unstaggering" the raw variables the wind components have an extra
      row and column relative to latitude, longitude.

--- wrf_python_demo.py
+++ wrf_python_demo.py
@@ -12,6 +12,13 @@ if __name__ == "__main__":
     files = glob(os.path.join(datadir, "wrfsees_ccs3pb1_ls2_d02_*"))
     nclist = [netCDF4.Dataset(f, mode="r") for f in files]
     smois = getvar(nclist, 'SMOIS', timeidx=ALL_TIMES)
+    smois = getvar(nclist, 'SMOIS', timeidx=ALL_TIMES)
+    uvmet = getvar(nclist, 'uvmet', timeidx=ALL_TIMES)
+    uvmet10 = getvar(nclist, 'uvmet10', timeidx=ALL_TIMES)
+    ua = getvar(nclist, 'ua', timeidx=ALL_TIMES)
+    va = getvar(nclist, 'va', timeidx=ALL_TIMES)
+    lat = getvar(nclist, 'lat', timeidx=ALL_TIMES)
+    lon = getvar(nclist, 'lon', timeidx=ALL_TIMES)
     zs = getvar(nclist, 'ZS', timeidx=ALL_TIMES, meta=False)[0, ...]
     dzs = getvar(nclist, 'DZS', timeidx=ALL_TIMES, meta=False)[0, ...]
     island = getvar(nclist, 'XLAND', timeidx=ALL_TIMES, meta=False)[0, ...]
