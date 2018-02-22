94d85d0d2dbc2a6fa123c0f0996686768bfb726f
Timothy W. Hilton <thilton@ucmerced.edu>
2018-02-22 15:45:00 -0700

    successfully test whether Big Basin is within the Redwoods range
    - needed to transform lat/lon coords to the same projection as Redwood
      Range map

--- RedwoodMap/test_in_redwood_range.py
+++ RedwoodMap/test_in_redwood_range.py
@@ -1,7 +1,10 @@
 import os.path
 import fiona
 from shapely.geometry import shape,mapping, Point, Polygon, MultiPolygon
+from pyproj import Proj, transform
 
+rwProj = Proj(init='epsg:3857')
+latlonProj = Proj(init='epsg:4326')
 fname = os.path.join('/', 'Users', 'tim', 'work', 'Data',
                      'Redwoods',
                      'Redwood_SequoiaSempervirens_extentNorthAmerica',
@@ -9,9 +12,9 @@ fname = os.path.join('/', 'Users', 'tim', 'work', 'Data',
 rr_polys = fiona.open(fname)  # Redwood range polygons
 rr_shapes = [shape(this_poly['geometry']) for this_poly in rr_polys]
 
-bbrwsp = Point(37.1737, -122.2464)  # Big Basin Redwoods State Park
-
-
+bbrwsp = Point(-122.2464, 37.1737)  # Big Basin Redwoods State Park
+x2, y2 = transform(latlonProj, rwProj, -122.2464, 37.1737)
+bbrwsp = Point(x2, y2)  # Big Basin Redwoods State Park
 
 for i, this_shape in enumerate(rr_shapes):
     in_poly = bbrwsp.within(this_shape)
