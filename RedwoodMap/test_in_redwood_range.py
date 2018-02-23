5ad5305715efecf9966a6f269418bd358aa5855d
Timothy W. Hilton <thilton@ucmerced.edu>
2018-02-23 10:13:44 -0700

    clean up test_in_redwood_range.py
    - remove unused code

--- RedwoodMap/test_in_redwood_range.py
+++ RedwoodMap/test_in_redwood_range.py
@@ -16,7 +16,6 @@ import wrf_grid
 
 fname_wrf = os.path.join('/', 'Users', 'tim', 'work', 'Data', 'WRF_Driver',
                          'met_em.d01.2009-06-01_00:00:00.nc')
-lonwrf_crnr, latwrf_crnr = wrf_grid.get_wrf_latlon(fname_wrf, 'XLONG_C', 'XLAT_C')
 lonwrf_ctr, latwrf_ctr = wrf_grid.get_wrf_latlon(fname_wrf, 'CLONG', 'CLAT')
 
 # def in_wrf_domain(lonwrf, latwrf):
@@ -31,25 +30,13 @@ rwProj = Proj(init='epsg:3857')
 latlonProj = Proj(init='epsg:4326')
 xwrf_ctr, ywrf_ctr = transform(latlonProj, rwProj,
                                lonwrf_ctr.data, latwrf_ctr.data)
-xwrf_crnr, ywrf_crnr = transform(latlonProj, rwProj,
-                                 lonwrf_crnr.data, latwrf_crnr.data)
-
-rw_polys = fiona.open(fname)  # Redwood range polygons
-rw_shapes = [shape(this_poly['geometry']) for this_poly in rw_polys]
-
-# wrf_cell_centers = Point(xwrf, ywrf)
-
-# for i, this_shape in enumerate(rw_shapes):
-#     in_poly = bbrwsp.within(this_shape)
-#     print("{:02d} {}".format(i, in_poly))
-
-
-
-
 
 # bbrwsp = Point(-122.2464, 37.1737)  # Big Basin Redwoods State Park
 # x2, y2 = transform(latlonProj, rwProj, -122.2464, 37.1737)
 # bbrwsp = Point(x2, y2)  # Big Basin Redwoods State Park
+
+rw_polys = fiona.open(fname)  # Redwood range polygons
+rw_shapes = [shape(this_poly['geometry']) for this_poly in rw_polys]
 
 has_redwoods = np.zeros(latwrf_ctr.shape, dtype=bool)
 for i in range(latwrf_ctr.shape[0]):
@@ -58,17 +45,11 @@ for i in range(latwrf_ctr.shape[0]):
         has_redwoods[i, j] = np.any([p.within(this_shape) for
                                      this_shape in rw_shapes])
 
-
-# def myPoint(x, y):
-#     return(Point(x, y))
-# pointvec = np.vectorize(myPoint)
-# p2 = np.empty(latwrf.shape, dtype=object)
-# p2 = pointvec(xwrf, ywrf)
-
 (lon_LL, lat_LL,
  lon_UL, lat_UL,
  lon_UR, lat_UR,
  lon_LR, lat_LR) = wrf_grid.get_cell_corner_coords(fname_wrf)
+x_LL, y_LL = transform(latlonProj, rwProj, lon_LL, lat_LL)
 
 fig = plt.figure(figsize=(6, 6))
 ax = plt.axes(projection=ccrs.PlateCarree())
@@ -81,10 +62,12 @@ states_provinces = cfeature.NaturalEarthFeature(
 ax.add_feature(states_provinces, edgecolor='grey')
 # ax.add_geometries(geoms=rw_shapes, crs=proj,
 #                   edgecolor='blue', facecolor='blue')
-ax.set_extent((lonwrf_ctr.min(), lonwrf_ctr.max(),
-               latwrf_ctr.min(), latwrf_ctr.max()))
+ax.set_extent((lon_LL.min(), lon_UR.max(),
+               lat_LL.min(), lat_UR.max()))
 has_redwoods = ma.masked_where(has_redwoods == False, has_redwoods)
-ax.pcolormesh(xwrf_crnr, ywrf_crnr, has_redwoods,
+
+ax.pcolormesh(x_LL, y_LL,
+              has_redwoods,
               transform=ccrs.Mercator.GOOGLE)
 ax.scatter(xwrf_ctr, ywrf_ctr,
            color='black', marker='x',
