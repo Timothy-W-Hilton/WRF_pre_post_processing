fa422eba3f468f7d41344245ae3fd1e95f710476
Timothy W. Hilton <thilton@ucmerced.edu>
2018-02-23 14:36:10 -0700

    factor out cross-referencing to new module and implement demo

---
+++ RedwoodMap/WRF_redwoods_grid_demo.py
@@ -0,0 +1,61 @@
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import os.path
+import wrf_grid
+import redwoods_shapes
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+from wrf_redwoods_crossref import main_wrapper
+
+def draw_map(gdf_wrf_all, gdf_wrf_rw, gdf_rw):
+    fig = plt.figure(figsize=(6, 6))
+    ax = plt.axes(projection=ccrs.PlateCarree())
+    ax.coastlines(resolution='10m')
+    states_provinces = cfeature.NaturalEarthFeature(
+        category='cultural',
+        name='admin_1_states_provinces_lines',
+        scale='10m',
+        facecolor='none')
+    ax.add_feature(states_provinces, edgecolor='black')
+    ax.add_geometries(geoms=gdf_wrf_rw.geometry, crs=ccrs.Mercator.GOOGLE,
+                      facecolor='#1b9e77', edgecolor='black', alpha=0.5,
+                      label='Redwood range')
+    ax.add_geometries(geoms=gdf_wrf_all.geometry, crs=ccrs.Mercator.GOOGLE,
+                      facecolor='white', edgecolor='black', alpha=0.3,
+
+                      label='WRF cells with redwoods')
+    ax.add_geometries(geoms=gdf_rw.geometry, crs=ccrs.Mercator.GOOGLE,
+                      facecolor='#d95f02', edgecolor='black', alpha=0.5,
+                      label='WRF cells with redwoods')
+    bounds = gdf_wrf.total_bounds
+    ax.set_extent(bounds[(0, 2, 1, 3), ], crs=ccrs.Mercator.GOOGLE)
+
+    # make proxy artists for a legend
+    redwoods = mpatches.Rectangle((0, 0), 1, 1, facecolor='#d95f02')
+    wrf_cells = mpatches.Rectangle((0, 0), 1, 1, facecolor='white',
+                                      edgecolor='black', alpha=0.3)
+    wrf_redwoods = mpatches.Rectangle((0, 0), 1, 1, facecolor='#1b9e77',
+                                   edgecolor='black', alpha=0.5)
+    labels = ['Redwoods range',
+              'WRF grid',
+              'WRF cells with redwoods']
+    plt.legend([redwoods, wrf_cells, wrf_redwoods], labels,
+               loc='lower left', bbox_to_anchor=(0.025, -0.1), fancybox=True)
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    fname_wrf = os.path.join('/', 'Users', 'tim', 'work', 'Data',
+                             'WRF_Driver',
+                             'met_em.d01.2009-06-01_00:00:00.nc')
+    fname_redwoods = os.path.join(
+        '/', 'Users', 'tim', 'work', 'Data', 'Redwoods',
+        'Redwood_SequoiaSempervirens_extentNorthAmerica',
+        'data', 'commondata', 'data0', 'sequsemp.shp')
+
+    (gdf_rw, gdf_wrf,
+     wrf_cells_with_redwoods,
+     redwoods_mask) = main_wrapper(fname_redwoods, fname_wrf)
+
+    draw_map(gdf_wrf, wrf_cells_with_redwoods, gdf_rw)
---
+++ RedwoodMap/wrf_redwoods_crossref.py
@@ -0,0 +1,63 @@
+"""determine which WRF cells contain redwoods
+"""
+
+import numpy as np
+import redwoods_shapes
+import geopandas as gpd
+from pyproj import Proj
+
+import os.path
+import wrf_grid
+
+def get_WRF_geodataframe(fname_wrf, proj=None):
+    gdf_wrf = wrf_grid.WRF_cells_to_gdf(fname_wrf, proj=proj)
+    return(gdf_wrf)
+
+def get_RW_geodataframe(fname_redwoods):
+    gdf_rw = redwoods_shapes.redwood_range_to_gdf(fname_redwoods)
+    return(gdf_rw)
+
+def get_wrf_cells_with_redwoods(gdf_wrf, gdf_rw):
+
+    gdf_wrf['has_redwoods'] = False
+    for i in range(gdf_rw.size):
+        gdf_wrf['has_redwoods'] = (
+            gdf_wrf['has_redwoods'] |
+            gdf_wrf.intersects(gdf_rw.iloc[i].geometry))
+    wrf_cells_with_redwoods = gdf_wrf[gdf_wrf['has_redwoods']]
+    # this should determine the shape of the intersection, it takes a
+    # lot longer than the above loop, which merely determines whether
+    # or not each WRF grid cell overlaps with any redwoods polygon
+    # UNTESTED
+    # cells_that_have_redwoods = gpd.overlay(gdf_wrf, gdf_rw, how='intersection')
+    return(gdf_wrf, wrf_cells_with_redwoods)
+
+def get_redwood_mask(gdf_wrf, wrf_cells_with_redwoods):
+    redwood_mask = np.zeros((gdf_wrf['x'].max(),
+                             gdf_wrf['y'].max()),
+                            dtype=bool)
+    redwood_mask[wrf_cells_with_redwoods['x'],
+                 wrf_cells_with_redwoods['y']] = True
+    return(redwood_mask)
+
+def main_wrapper(fname_redwoods, fname_wrf):
+    rwProj = Proj(init='epsg:3857')
+    gdf_rw = get_RW_geodataframe(fname_redwoods)
+    gdf_wrf = get_WRF_geodataframe(fname_wrf, proj=rwProj)
+    gdf_wrf, wrf_cells_with_redwoods = get_wrf_cells_with_redwoods(gdf_wrf,
+                                                                   gdf_rw)
+    redwoods_mask = get_redwood_mask(gdf_wrf, wrf_cells_with_redwoods)
+    return(gdf_rw, gdf_wrf, wrf_cells_with_redwoods, redwoods_mask)
+
+if __name__ == "__main__":
+    fname_wrf = os.path.join('/', 'Users', 'tim', 'work', 'Data',
+                             'WRF_Driver',
+                             'met_em.d02.2009-06-01_00:00:00.nc')
+    fname_redwoods = os.path.join(
+        '/', 'Users', 'tim', 'work', 'Data', 'Redwoods',
+        'Redwood_SequoiaSempervirens_extentNorthAmerica',
+        'data', 'commondata', 'data0', 'sequsemp.shp')
+
+    (gdf_rw, gdf_wrf,
+     wrf_cells_with_redwoods,
+     redwoods_mask) = main_wrapper(fname_redwoods, fname_wrf)
