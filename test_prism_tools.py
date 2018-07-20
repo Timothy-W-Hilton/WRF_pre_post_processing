8dfef8e6f0fd098f26dc439bbff76e29642708a1
Timothy W. Hilton <thilton@ucmerced.edu>
2018-07-20 11:05:40 -0600

    pickle parsed PRISM data for faster testing of interpolation

--- test_prism_tools.py
+++ test_prism_tools.py
@@ -1,15 +1,28 @@
 import os.path
 import prism_tools
+import pickle
+
+parse_data = False
 
 if __name__ == "__main__":
     prism_dir = os.path.join('/', 'Users',
                              'tim', 'work', 'Data', 'PRISM')
-    pts = prism_tools.PRISMTimeSeries(
-        os.path.join(prism_dir,
-                     'PRISM_tmean_stable_4kmD1_20090601_20090630_asc.zip'),
-        varname='tmean', varunits='C')
-    pts.from_zip_archive()
+    fname_pickle = "pts.pickle"
+    if parse_data:
 
-    lat, lon = prism_tools.read_WRF_latlon(
+        pts = prism_tools.PRISMTimeSeries(
+            os.path.join(prism_dir,
+                         'PRISM_tmean_stable_4kmD1_20090601_20090630_asc.zip'),
+            varname='tmean', varunits='C')
+        pts.from_zip_archive()
+        pfile = open(fname_pickle, 'wb')
+        pickle.dump(pts, pfile)
+        pfile.close()
+    else:
+        pfile = open(fname_pickle, 'rb')
+        pts = pickle.load(pfile)
+        pfile.close()
+
+    lon, lat = prism_tools.read_WRF_latlon(
         os.path.join(prism_dir, 'WRF_d02_latlon.nc'))
-    pts.interpolate()
+    # new_image = pts.interpolate(lon, lat)
