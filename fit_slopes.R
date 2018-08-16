a93aa0b2dd409a3e1adc015594c0365846779822
Timothy W. Hilton <thilton@ucmerced.edu>
2018-08-15 20:06:59 -0600

    fix matrix transposed error; clean up syntax
    - matrices parsed by raster::brick were winding up transposed and
      flipped vertically
    - end functions with return() rather than depend on R returning the
      last statement in the function

--- fit_slopes.R
+++ fit_slopes.R
@@ -1,16 +1,26 @@
 library(ncdf4)
 library(raster)
 
+## convert Kelvins to centigrade
+##
 K_to_C <- function(K) {
     return(K - 273.15)
 }
 
+## helper function for raster::calc
+##
+## The main purpose is to handle (by ignoring them) NAs in the data to
+## be fit
+##
+## adapted from
+## https://stackoverflow.com/questions/32975210/linear-regression-on-raster-images-lm-complains-about-nas/
+## 15 Aug 2018
 linear_fitter <- function(y) {
     if(all(is.na(y))) {
-        c(NA, NA)
+        return(c(NA, NA))
     } else {
         x <- 1:nlayers(dT)
-        lm(y ~ x)$coefficients
+        return(lm(y ~ x)$coefficients)
     }
 }
 
@@ -23,19 +33,25 @@ gb <- list(xmn=-133.7434844970703, xmx=-116.14889526367188,
 nc <- nc_open('PRISM_tmean.nc')
 lon <- ncvar_get(nc, 'lon')
 lat <- ncvar_get(nc, 'lat')
-Tmean_prism  <- brick(ncvar_get(nc, 'tmean'),
-                      crs=proj4_str,
-                      xmn=gb[['xmn']], xmx=gb[['xmx']],
-                      ymn=gb[['ymn']], ymx=gb[['ymx']])
+Tmean_prism  <- flip(brick(ncvar_get(nc, 'tmean'),
+                           crs=proj4_str,
+                           xmn=gb[['xmn']], xmx=gb[['xmx']],
+                           ymn=gb[['ymn']], ymx=gb[['ymx']],
+                           transpose=TRUE),
+                     direction='y')
 nc_close(nc)
 
 nc <- nc_open('nourbanNOAH_d02_T.nc')
-Tmean_WRFNOAH  <- brick(ncvar_get(nc, 'T2'),
-                        crs=proj4_str,
-                        xmn=gb[['xmn']], xmx=gb[['xmx']],
-                        ymn=gb[['ymn']], ymx=gb[['ymx']])
+Tmean_WRFNOAH  <- flip(brick(ncvar_get(nc, 'T2'),
+                             crs=proj4_str,
+                             xmn=gb[['xmn']], xmx=gb[['xmx']],
+                             ymn=gb[['ymn']], ymx=gb[['ymx']],
+                             transpose=TRUE),
+                       direction='y')
+
 Tmean_WRFNOAH <- K_to_C(Tmean_WRFNOAH)
 nc_close(nc)
 
 dT <- Tmean_prism - Tmean_WRFNOAH
 fits <- raster::calc(dT, linear_fitter)
+names(fits) <- c('slope', 'intercept')
