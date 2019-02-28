d1dde0ed730b6fc05926d4b1faac9aff56a72895
Timothy W. Hilton <thilton@ucmerced.edu>
2019-02-27 19:12:55 -0800

    commit a few driver scripts to work on during plane trip

--- plot_diff_driver_nourbanCLM_ctl.py
+++ plot_diff_driver_nourbanCLM_ctl.py
@@ -1,8 +1,7 @@
 import datetime
 import os
-import numpy.ma as ma
+import numpy as np
 from plot_diff import var_diff, VarDiffPlotter
-import means_diff_test
 
 DOMAIN = 2
 
@@ -11,32 +10,48 @@ if __name__ == "__main__":
 
     cscratchdir = os.path.join('/', 'global', 'cscratch1', 'sd',
                                'twhilton')
-    rootdir = os.path.join(cscratchdir, 'WRFv3.9_Sensitivity')
-    ctl_dir = os.path.join(rootdir, 'WRFV3_Ctl',
-                           'run', 'summen_sensitivity_ctl')
-    urb_dir = os.path.join(rootdir, 'WRFV3_redwoodsurban',
-                           'run', 'summen_redwoodsurban')
+    rootdir = os.path.join(cscratchdir, 'WRFv4.0_Sensitivity')
+    ctl_dir = os.path.join(rootdir, 'WRFCLMv4.0_NCEPDOEp2', 'WRFV4',
+                           'run', 'summen_2005_ctl_NCEPDOE')
+    nourb_dir = os.path.join(rootdir,
+                             'WRFCLMv4.0_NCEPDOEp2_deurbanized', 'WRFV4',
+                             'run', 'summen_2005_deurbanized_NCEPDOE')
     out_dir = os.path.join(cscratchdir, 'plots_temporary')
 
-    # insignificant_pixels = ma.masked_invalid(means_diff_test.main(0.95)).mask
-
-    varname = 'fogpresent'
+    # varname = 'HFX'
+    # varname = 'LH';
+    # varname = 'LCL'
+    # varname = 'fogpresent'
+    # varname = 'SMOIS'
+    # varname = 'fogbase'
+    # varname = 'fogpct'
+    # varname = 'QCLOUD'
+    # varname = 'uvmet10'
+    varname = 'wa'
+    # varname = 'ctt'  # cloud top temperature
+    # varname = 'LU_INDEX'
 
     read_data = False
     if read_data:
         # wildcard_pat = "*d{:02d}02_2009-06-\{0[0-9],1[01]\}".format(DOMAIN)
-        wildcard_pat = "*d{:02d}_2009-06-*".format(DOMAIN)
+        wildcard_pat = "*d{:02d}_2005-*".format(DOMAIN)
         vd = var_diff(os.path.join(ctl_dir, wildcard_pat),
-                      os.path.join(urb_dir, wildcard_pat),
+                      os.path.join(nourb_dir, wildcard_pat),
                       label_A='ctl',
-                      label_B='urbanRW',
+                      label_B='no_urban_CLM',
                       varname=varname)
         vd.read_files()
+        # for k in vd.data.keys():
+        #     z_ax = vd.var_axes.index('Lay')
+        #     idx = [slice(None), ] * vd.data[k].ndim
+        #     idx[z_ax] = slice(0, 10)
+        #     vd.data[k] = np.mean(vd.data[k][idx], axis=z_ax)
+        # vd.var_axes.pop(vd.var_axes.index('Lay'))
         vd.get_significance_mask(significance=0.95, adj_autocorr=True)
         vd.to_netcdf(os.path.join('/', 'global', 'cscratch1', 'sd',
                                   'twhilton',
-                                  '{}_d{:02d}_RWurban.nc'.format(varname,
-                                                                 DOMAIN)))
+                                  '{}_d{:02d}_CLM_nourban.nc'.format(varname,
+                                                                     DOMAIN)))
         print('done reading files ({})'.format(datetime.datetime.now() - t0))
         # vd.mask_land_or_water(mask_water=False)
     else:
@@ -44,36 +59,38 @@ if __name__ == "__main__":
             ncfile=os.path.join(
                 cscratchdir,
                 # '/Users/tim/work/Data/SummenWRF/',
-                '{varname}_d{DOMAIN:02d}_RWurban.nc'.format(
+                '{varname}_d{DOMAIN:02d}_CLM_nourban.nc'.format(
                     varname=varname, DOMAIN=DOMAIN)))
+        # for k in vd.data.keys():
+        #     vd.data[k] = vd.data[k] * 100.0
         if vd.p is None:
             vd.get_significance_mask(significance=0.95, adj_autocorr=True)
     # vd = is_foggy_obrien_2013(vd)
-    pfx = '7day_urban'
+    pfx = 'nourbanCLM_95CI'
     # for this_series in ['all_tstamps', 'time_avg']:
     for this_series in ['time_avg']:
         if this_series == 'all_tstamps':
             t_end = 1
-            pfx = 'redwoodsurban'
+            pfx = 'nourbanCLM'
             time_title_str = None
         else:
             t_end = 1
             pfx = pfx + '_timeavg'
             vd.aggregate_time(time_avg=True)
-            time_title_str = 'June 2009'
+            time_title_str = 'June 2005'
         for this_t in range(0, t_end):  #
             plotter = VarDiffPlotter(vd, t_idx=this_t, layer=0,
                                      domain=DOMAIN,
                                      pfx=pfx,
-                                     savedir=out_dir,
+                                     savedir='.',
                                      time_title_str=time_title_str)
             if DOMAIN == 1:
                 cb_orientation = 'horizontal'
             else:
                 cb_orientation = 'vertical'
             fig = plotter.plot(cb_orientation=cb_orientation,
-                               mask=(vd.p < 0.95))
-                               # vmin=-0.0000001,
-                               # vmax=1.0000001)
+                               vmin=None,
+                               vmax=None,
+                               mask=vd.p > 0.05)
 
     print('done driver ({})'.format(datetime.datetime.now() - t0))
--- plot_diff_wind_test.py
+++ plot_diff_wind_test.py
@@ -19,10 +19,12 @@ if __name__ == "__main__":
 
     out_dir = os.path.join(cscratchdir, 'plots_temporary')
 
-    varname = 'wa'
     varname = 'uvmet'
+    varname = 'uvmet10'
+    # varname = 'wa'
+    # varname = 'ctt'  # cloud top temperature
 
-    read_data = True
+    read_data = False
     if read_data:
         # wildcard_pat = "*d{:02d}02_2009-06-\{0[0-9],1[01]\}".format(DOMAIN)
         wildcard_pat = "*d{:02d}_2005-06-0*".format(DOMAIN)
@@ -32,16 +34,17 @@ if __name__ == "__main__":
                       label_B='drysoil_CLM',
                       varname=varname)
         vd.read_files()
-        for k in vd.data.keys():
-            z_ax = vd.var_axes.index('Lay')
-            idx = [slice(None), ] * vd.data[k].ndim
-            idx[z_ax] = slice(0, 10)
-            vd.data[k] = np.mean(vd.data[k][idx], axis=z_ax)
-        vd.var_axes.pop(vd.var_axes.index('Lay'))
-        # vd.to_netcdf(os.path.join('/', 'global', 'cscratch1', 'sd',
-        #                           'twhilton',
-        #                           '{}_d{:02d}_CLM_drysoil.nc'.format(varname,
-        #                                                              DOMAIN)))
+        # for k in vd.data.keys():
+        #     z_ax = vd.var_axes.index('Lay')
+        #     idx = [slice(None), ] * vd.data[k].ndim
+        #     idx[z_ax] = slice(0, 10)
+        #     vd.data[k] = np.mean(vd.data[k][idx], axis=z_ax)
+        # vd.var_axes.pop(vd.var_axes.index('Lay'))
+        # vd.get_significance_mask(significance=0.95, adj_autocorr=True)
+        vd.to_netcdf(os.path.join('/', 'global', 'cscratch1', 'sd',
+                                  'twhilton',
+                                  '{}_d{:02d}_CLM_drysoil.nc'.format(varname,
+                                                                     DOMAIN)))
         print('done reading files ({})'.format(datetime.datetime.now() - t0))
     else:
         vd = var_diff(
@@ -53,13 +56,15 @@ if __name__ == "__main__":
     # vd = is_foggy_obrien_2013(vd)
     # vd.get_significance_mask(significance=0.95, adj_autocorr=True)
 
-    pfx = 'drysoilCLM'
+    pfx = 'TEST_drysoilCLM'
     # for this_series in ['all_tstamps', 'time_avg']:
-    for this_series in ['time_avg']:
+    for this_series in ['single']:
         if this_series == 'all_tstamps':
             t_end = 1
             pfx = 'drysoilCLM'
             time_title_str = None
+        elif this_series == 'single':
+            t_end = 200
         else:
             t_end = 1
             pfx = pfx + '_timeavg'
@@ -69,7 +74,9 @@ if __name__ == "__main__":
                 for k in vd.data.keys():
                     vd.data[k] = vd.data[k] * 100
             time_title_str = 'June 2005'
-        for this_t in range(0, t_end):  #
+        for this_t in range(10, t_end):  #
+            if this_series == 'single':
+                time_title_str = vd.time[this_t].strftime('%Y-%m-%d_%H%M')
             plotter = VarDiffPlotter(vd, t_idx=this_t, layer=0,
                                      domain=DOMAIN,
                                      pfx=pfx,
@@ -82,6 +89,6 @@ if __name__ == "__main__":
             fig = plotter.plot(cb_orientation=cb_orientation,
                                vmin=None,
                                vmax=None,
-                               mask=vd.insignificant_mask)
+                               mask=None)
 
     print('done driver ({})'.format(datetime.datetime.now() - t0))
