d064394109a95fb72092ee34cd0015e3fd7fbb52
Timothy W. Hilton <thilton@ucmerced.edu>
2019-06-25 09:36:39 -0700

    test out xarray file read with 200 files, variable exclusion
      - seems to scale linearly, slope a little more than 1.0:
        - 50 files - 8 seconds
        - 100 files - 21 seconds
        - 200 files - 44 seconds
        - this means ~5000 will take about ~18 minutes... still much too
          slow for on-the-fly plotting

--- test_xarray_many_files.py
+++ test_xarray_many_files.py
@@ -5,6 +5,8 @@ import re
 import wrf
 import netCDF4
 
+n_files = 200
+
 if __name__ == "__main__":
 
     wrf_dir = os.path.join('/', 'global', 'cscratch1', 'sd', 'twhilton',
@@ -21,23 +23,29 @@ if __name__ == "__main__":
 
     t0 = datetime.datetime.now()
     print('starting xarray.open_mfdataset read ({})'.format(t0))
-    ds = xr.open_mfdataset(matching_files)
-    print('done xarray.open_mfdataset ({})'.format(
+    ds = xr.open_mfdataset(matching_files[0])
+    print('done opening first file'.format(
         datetime.datetime.now() - t0))
 
     t0 = datetime.datetime.now()
-    print('starting HFX read ({})'.format(t0))
-    hfx = ds['HFX'].values
-    print('done HFX read ({})'.format(
+    all_vars = list(ds.data_vars.keys())
+    ds.close()
+    keep_vars = ['Times', 'HFX', 'LH', 'QCLOUD']
+    drop_vars = set(all_vars) - set(keep_vars)
+    print('starting xarray.open_mfdataset read (with drop) ({})'.format(t0))
+    ds = xr.open_mfdataset(matching_files[:n_files],
+                           drop_variables=drop_vars)
+    print('done xarray.open_mfdataset ({}) ({})'.format(
+        n_files,
         datetime.datetime.now() - t0))
 
-    # this doesn't work
-    fname = 'HFX_d02_all.nc'
-    fname = 'foo1day.nc'
-    fname = 'metem_2005_ctl_NCEPDOE_d02_2005-06-01_00:00:00.nc'
-    print('starting {} read ({})'.format(fname, t0))
-    bigfile = netCDF4.Dataset(os.path.join(wrf_dir, fname), 'r')
-    wrfv = wrf.getvar(bigfile, 'HFX', wrf.ALL_TIMES)
-    # netcdf4v = bigfile.variables['HFX'][...]
-    bigfile.close()
-    print('done {} ({})'.format(fname, datetime.datetime.now() - t0))
+    # # this doesn't work
+    # fname = 'HFX_d02_all.nc'
+    # fname = 'foo1day.nc'
+    # fname = 'metem_2005_ctl_NCEPDOE_d02_2005-06-01_00:00:00.nc'
+    # print('starting {} read ({})'.format(fname, t0))
+    # bigfile = netCDF4.Dataset(os.path.join(wrf_dir, fname), 'r')
+    # wrfv = wrf.getvar(bigfile, 'HFX', wrf.ALL_TIMES)
+    # # netcdf4v = bigfile.variables['HFX'][...]
+    # bigfile.close()
+    # print('done {} ({})'.format(fname, datetime.datetime.now() - t0))
