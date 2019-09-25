44796abb12d3b8accb5aeb40c6a0635859a990cc
Timothy W. Hilton <twhilton@ucsc.edu>
2019-09-25 11:36:06 -0600

    plot sensible heat and latent heat, move plotting code from jupyter notebook
    - helper functions are now in geoviews_tools.py

--- geoviews_tools.py
+++ geoviews_tools.py
@@ -2,8 +2,27 @@ import numpy as np
 import os
 import xarray as xr
 import holoviews as hv
+from holoviews import opts
 import geoviews as gv
 import geoviews.feature as gf
+
+from adjust_WRF_inputs import km_to_yatir
+
+
+def yatir_mask(xarr, d_threshold=5):
+    """add a variable identifying WRF cells in Yatir parameterization
+
+    The new variable is boolean, True for cells in Yatir, False otherwise.
+
+    ARGS:
+    xarr [xarray.Dataset]: dataset containing Yatir WRF data.  Must
+       contain variables 'lon' and 'lat'
+    d_threshold [int]: distance threshold (in kilometers) to be
+       considered "in Yatir".  Default is 5 km.
+    """
+    d_to_yatir = km_to_yatir(xarr['lat'].values, xarr['lon'].values)
+    xarr['yatir_mask'] = (('x', 'y'), d_to_yatir < d_threshold)
+    return(xarr)
 
 
 def yatir_to_xarray(fname, varname, groupname=None, timerange=None):
@@ -30,6 +49,7 @@ def yatir_to_xarray(fname, varname, groupname=None, timerange=None):
                      attrs={'varname': varname,
                             'groupname': groupname,
                             'units': ds_all.units})
+    var = yatir_mask(var)
     return(var)
 
 
@@ -68,6 +88,47 @@ def build_geoviews_comparison(ds_d02, ds_d03, pad=[1.0, 0.1]):
                          group=ds_d03.groupname,
                          label=ds_d03.varname)
     return(map_d02, map_d03)
+
+
+def xrvar_to_hvdim(var):
+    """create a HoloViews dimension from an xarray variable
+
+    Assumes that the xarray variable has attributes name, description,
+    and units
+
+    RETURNS:
+        hv.Dimension object
+
+    """
+    try:
+        return(hv.Dimension(var.name, label=var.description, unit=var.units))
+    except AttributeError:
+        print('var must have attributes name, description, and units')
+        raise
+
+
+def overlay_roughness_realization_timeseries(dsxr, varname):
+    """create a HoloViews Overlay showing one variable's timeseries
+
+    to create a timeseries from two-dimensional data, the data are
+    averaged spatially before plotting
+
+    RETURNS:
+      hvOverlay object containing timeseries for all WRF runs in the experiment
+
+    """
+    df_var = dsxr.mean(dim=['west_east', 'south_north'],
+                       skipna=True)[varname].to_dataframe()
+    vert_dim = xrvar_to_hvdim(dsxr[varname])
+    tbl_list = [hv.Table(df[1], label=df[0]) for
+                df in df_var.groupby('WRFrun')]
+    crv_list = [hv.Curve(tbl, kdims='Time', vdims=vert_dim) for
+                tbl in tbl_list]
+    OLopts = opts.Overlay(frame_width=690, legend_position='right')
+    label = 'roughness length experiments: {}'.format(
+        dsxr[varname].description)
+    hvol = hv.Overlay(crv_list).opts(OLopts).relabel(label)
+    return(hvol)
 
 
 if __name__ == '__main__':
