7921487d87821bb1f23028df69a7ebe544f9087f
Timothy W. Hilton <thilton@ucmerced.edu>
2020-01-10 09:07:14 -0800

    commit wrf_postprocessor.py
    - calculates derived variables height, height_agl, potential
    temperature, combines them with selected WRF output variables and
    writes them to a netCDF file that, for example, data visualization
    code can read concisely.

---
+++ wrf_postprocessor.py
@@ -0,0 +1,68 @@
+import os
+import xarray as xr
+import netCDF4
+import wrf
+
+
+def create_merged_postprocessed_netcdf(collected_data_directory,
+                                       allvars_fname,
+                                       vars_derived,
+                                       vars_raw,
+                                       run_key):
+
+    fname = os.path.join(collected_data_directory,
+                         allvars_fname[run_key])
+    nc = netCDF4.Dataset(filename=fname, mode='r')
+    ds_derived = xr.Dataset({k: wrf.getvar(wrfin=nc,
+                                           varname=k,
+                                           timeidx=wrf.ALL_TIMES)
+                             for k in vars_derived})
+    nc.close()
+
+    # format variable metadata for compatibility with
+    # xarray.Dataset.to_netcdf().
+    for this_var in ds_derived.data_vars:
+        for k, v in ds_derived[this_var].attrs.items():
+            if type(v) is wrf.projection.LambertConformal:
+                # cast projection information into string -
+                # wrf.projection objects throw an error in to_netcdf()
+                ds_derived[this_var].attrs[k] = str(v)
+            elif k == 'coordinates':
+                # (2) rename variables' "coordinates" attribute to
+                # 'coordinates_attr'. See
+                # https://github.com/pydata/xarray/issues/1809)
+                ds_derived[this_var].attrs[
+                    'coordinates_attr'] = ds_derived[this_var].attrs.pop(k)
+
+    ds_raw = xr.open_dataset(fname)
+    # set(vars_raw)) list ds_raw first and set compat to override
+    # because getvar() replaces string timestamps with numerical
+    # timestamps, and merge() then sees that ds_raw has strings in
+    # XTIME and ds_derived has numbers and decides they are
+    # incompatible.  setting compat to override keeps the values from
+    # the first dataset (the strings in raw).
+    ds_combined = xr.merge([ds_raw[vars_raw], ds_derived], compat='override')
+    ds_combined.to_netcdf(path=os.path.join(
+        os.path.dirname(fname),
+        '{}_d03_postprocessed.nc'.format(run_key)))
+
+
+if __name__ == "__main__":
+
+    collected_data_directory = os.path.join('/', 'global',
+                                            'cscratch1', 'sd', 'twhilton',
+                                            'yatir_output_collected')
+    allvars_fname = {'ytr': 'ytr_d03_allvars.nc',
+                     'ctl': 'ctl_d03_allvars.nc'}
+
+    vars_derived = ['height', 'height_agl', 'uvmet_wspd_wdir',
+                    'uvmet10_wspd_wdir', 'theta']
+    vars_raw = ['Times', 'XLONG', 'XLAT', 'HFX', 'LH', 'FIRA', 'FSA',
+                'GPP', 'SWDOWN', 'FVEG', 'RSSUN', 'RSSHA', 'W', 'T', 'P']
+
+    for k in ['ytr', 'ctl']:
+        create_merged_postprocessed_netcdf(collected_data_directory,
+                                           allvars_fname,
+                                           vars_derived,
+                                           vars_raw,
+                                           k)
