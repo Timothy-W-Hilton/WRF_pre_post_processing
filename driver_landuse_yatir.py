623a6e3009b0b8292dd048d328aa2352f80c60c8
Timothy W. Hilton <thilton@ucmerced.edu>
2020-03-11 13:36:00 -0700

    try adjusting soil moisture by factor of 1.1
    - factor of 2.0 triggers a "gaining water error"

--- driver_landuse_yatir.py
+++ driver_landuse_yatir.py
@@ -12,14 +12,23 @@ Yatir Forest eddy covariance tower () and for those cells:
 """
 
 import os
-from adjust_WRF_inputs import use_yatir_parameterization, gather_files
+from adjust_WRF_inputs import use_yatir_parameterization
+from adjust_WRF_inputs import replace_landusef_luindex
+from adjust_WRF_inputs import gather_files
+from adjust_WRF_inputs import adjust_soil_moisture
 
 if __name__ == "__main__":
-    wrf_run_dir = os.path.join(
-        '/', 'global', 'cscratch1', 'sd',
-        'twhilton', 'WRFv4.1_Experiments',
-        'WRFv4.1_yatir_2015Aug_yatirparams_Z050_NCEPFNL_NOAHMP_ndom3_diagnostic',
-        'WRFV4', 'run')
+    # wrf_run_dir = os.path.join(
+    #     '/', 'global', 'cscratch1', 'sd',
+    #     'twhilton', 'WRFv4.1_Experiments',
+    #     'WRFv4.1_yatir_2015Aug_yatirparams_NCEPFNL_NOAHMP_ndom3_wetsoil',
+    #     'WRFV4', 'run')
+
+    VWC_factor = 1.1  # adjustment factor for soil moisture
+                      # (i.e. volumetric water content)
+
+    # directory of files for testing
+    wrf_run_dir = '/global/cscratch1/sd/twhilton/test_adjust_land/'
 
     wrfinput_files = sorted(gather_files(wrf_run_dir, "wrfinput*"))
     wrfbdy_files = sorted(gather_files(wrf_run_dir, "wrfbdy*"))
@@ -28,17 +37,28 @@ if __name__ == "__main__":
     met_em_files_d02 = sorted(gather_files(wrf_run_dir, "met_em.d02*"))
     met_em_files_d03 = sorted(gather_files(wrf_run_dir, "met_em.d03*"))
 
-    # for this_file in met_em_files_d01:
-    #     print("starting met_em")
-    #     use_yatir_parameterization(this_file)
-    # for this_file in met_em_files_d02:
-    #     use_yatir_parameterization(this_file)
-    # for this_file in met_em_files_d03:
-    #     use_yatir_parameterization(this_file)
+    for this_file in met_em_files_d01:
+        print("starting met_em")
+        use_yatir_parameterization(this_file)
+    for this_file in met_em_files_d02:
+        use_yatir_parameterization(this_file)
+        adjust_soil_moisture(this_file, VWC_factor, yatir_only=True)
+    for this_file in met_em_files_d03:
+        use_yatir_parameterization(this_file)
+        adjust_soil_moisture(this_file, VWC_factor, yatir_only = True)
     print("starting wrfinput")
-    for this_file in wrfinput_files:
-        use_yatir_parameterization(this_file)
-    for this_file in wrfbdy_files:
-        use_yatir_parameterization(this_file)
-    for this_file in wrflow_files:
-        use_yatir_parameterization(this_file)
+    # for this_wrfinput_file, this_metem_set in zip(wrfinput_files,
+    #                                               (met_em_files_d01,
+    #                                                met_em_files_d02,
+    #                                                met_em_files_d03)):
+    #     use_yatir_parameterization(this_wrfinput_file)
+    #     replace_landusef_luindex(this_wrfinput_file,
+    #                              this_metem_set[0])
+    #     adjust_soil_moisture(this_wrfinput_file,
+    #                          VWC_factor,
+    #                          yatir_only=True,
+    #                          soil_moist_vars=('SMOIS', ))
+    # for this_file in wrfbdy_files:
+    #     use_yatir_parameterization(this_file)
+    # for this_file in wrflow_files:
+    #     use_yatir_parameterization(this_file)
